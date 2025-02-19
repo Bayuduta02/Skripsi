a1a970e1e977dcec83bc59a37bd8561e48217619
Bayu Duta Firmansyah <bayuduta02@gmail.com>
2025-02-19 17:25:42 +0700

    Skripsiku

---
+++ app/src/main/java/com/example/skripsi/ImagePagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.skripsi
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImagePagerAdapter(private val imageList: List<Int>) : RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.bind(imageList[position])
+    }
+
+    override fun getItemCount(): Int = imageList.size
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.imageView)
+
+        fun bind(imageRes: Int) {
+            imageView.setImageResource(imageRes)
+            imageView.scaleType = ImageView.ScaleType.FIT_XY // Gambar tidak akan terpotong
+        }
+    }
+}
